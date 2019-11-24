620e48aef21af6ed1a256e81ca7190c5a8e4db33
Avisenna Abimanyu <theknightmare07@gmail.com>
2019-11-25 05:56:12 +0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+
+    margin: theme.spacing(3),
+
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
+  const inputFile = (e) => {
+    let files = e.target.files;
+    console.log(files)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="upper-container">
+        <div>
+          <p className="container-title">Source Image</p>
+          <div className="image-container">
+
+            <img alt="image source" src="" />
+          </div>
+        </div>
+        <div>
+          <p className="container-title">Detection Image</p>
+          <div className="image-container">
+
+            <img alt="shape" src="" />
+          </div>
+        </div>
+      </div>
+      <div className="lower-container">
+        <div>
+          <p className="container-title">Detection Result</p>
+          <div className="image-container">
+
+
+          </div>
+        </div><div>
+          <p className="container-title">Matched Facts</p>
+          <div className="image-container">
+
+
+          </div>
+        </div><div>
+          <p className="container-title">Hit Rules</p>
+          <div className="image-container">
+
+
+          </div>
+        </div>
+      </div>
+      <div className="button-container">
+        <input type="file" name="Open Image" onChange={(e) => inputFile(e)} />
+        <Button className={classes.root} variant="contained" color="primary">
+          Open Rule Editor
+      </Button>
+        <Button className={classes.root} variant="contained" color="secondary">
+          Show Rules
+      </Button>
+        <Button className={classes.root} variant="contained" color="primary">
+          Show Facts
+      </Button>
+      </div>
     </div>
   );
 }
